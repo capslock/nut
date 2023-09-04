24b4154668529affc79ebeb5e4dda0a50dbc6ba5
Richard Otap <richard.g.otap@gmail.com>
2023-09-04 09:44:23 -0700

    module: Set default `schedulerRules`.

--- flake.nix
+++ flake.nix
@@ -194,6 +194,9 @@
             schedulerRules = mkOption {
               example = "/etc/nixos/upssched.conf";
               type = types.str;
+              default = pkgs.writeText "upssched.conf" ''
+                CMDSCRIPT ${nut_pkgs}/bin/upssched-cmd
+              '';
               description = lib.mdDoc ''
                 File which contains the rules to handle UPS events.
               '';
